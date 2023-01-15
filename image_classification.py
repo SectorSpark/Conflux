d0697886db1d2ca8c6319bfd9cfb31a7d97d49fd
Yana_N <122021948+YanaNozhnina@users.noreply.github.com>
2023-01-15 22:46:35 +0600

    Update image_classification.py

--- image_classification.py
+++ image_classification.py
@@ -5,15 +5,37 @@ from tensorflow.keras.preprocessing import image
 import os
 import tensorflow as tf
 import tensorflow_hub as hub
+from PIL import Image
+
+# Проверка размера изображения: для обрезки должно быть не менее 256, для быстрой обработки также желательно ограничить максимальный размер 
+def check_size(shape):
+  if (shape[1] >= 256) and (shape[2] >= 256) and (shape[1] < 4000) and (shape[2] < 4000):
+    return True
+  else:
+    st.write('Error. Size must be > 256 and < 4000.')
+    return False
+#  
+def check_transparency(image):
+  pixels = image.convert('RGBA')
+  width, height = image.size
+  for i in range(width):
+      for j in range(height):
+          r,g,b,a = pixels.getpixel((i,j))
+          if a > 0:
+            return False
+            st.write('Error. Image must not contain transparent pixels')
+          else:
+            return True
 
 def crop_center(image):
   shape = image.shape
-  new_shape = min(shape[1], shape[2])
-  offset_y = max(shape[1] - shape[2], 0) // 2
-  offset_x = max(shape[2] - shape[1], 0) // 2
-  image = tf.image.crop_to_bounding_box(
-      image, offset_y, offset_x, new_shape, new_shape)
-  return image
+  if check_size(shape) == True:
+    new_shape = min(shape[1], shape[2])
+    offset_y = max(shape[1] - shape[2], 0) // 2
+    offset_x = max(shape[2] - shape[1], 0) // 2
+    image = tf.image.crop_to_bounding_box(
+        image, offset_y, offset_x, new_shape, new_shape)
+    return image
 
 def preprocess_image(img, resize):
     img = tf.io.decode_image(img, channels=3, dtype=tf.float32)[tf.newaxis, ...]
@@ -32,17 +54,20 @@ def load_image(imageLabel):
 
 
 st.title('Стилизация изображений')
-img = load_image('Первое изображение')
-img2 = load_image('Второе изображение') # стоит рассмотреть вариант расположения картинок по бокам а не друг над другом
+col1, col2 = st.columns(2)
+with col1:
+  img = load_image('Первое изображение')    
+with col2:
+  img2 = load_image('Второе изображение') 
 result = st.button('Применить стиль второго изображения к первому')
+
 if result:
-    # разрешение content_imgae лучше поменять чтобы соотношение сторон было всегда как в исходном файле, иначе он картинку обрезает
     content_image = preprocess_image(img,(2048,2048))
     style_image = preprocess_image(img2,(256,256))
     style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
     st.write('**Результат:**')
     hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
-    hub_module = hub.load(hub_handle) # наверное стоит грузить модель заранее, а не каждый раз когда нажимаешь кнопку
+    hub_module = hub.load(hub_handle) 
     outputs = hub_module(tf.constant(content_image), tf.constant(style_image))
     stylized_image = np.squeeze(outputs[0])
     st.image(stylized_image)
